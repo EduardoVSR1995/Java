226643df9413e11ee8b98b5d42ffd4b1983fd641
EduardoVSR1995 <eduardvitor7@gmail.com>
2023-03-21 00:00:03 -0300

    feat: add exercice DuplicadosNao

---
+++ DuplicadosNao/README.md
@@ -0,0 +1,4 @@
+## Exercicio:
+Duplicados não!
+## Descrição:
+Código que encontra os elementos em comum que existem em dois arrays e imprima os nomes dos itens em comum!
---
+++ DuplicadosNao/src/DuplicadosNao.java
@@ -0,0 +1,8 @@
+public class DuplicadosNao{
+    int[] array1 = new int[]{1,2,3,4,5,6,7,44,55,66,77}; 
+    int[] array2 = new int[]{12,324,53,74,85,96,17,44,55,66,77}; 
+    public static void main(String[] args) throws Exception {
+        
+        System.out.println("Hello, World!");
+    }
+}
