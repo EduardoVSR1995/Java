a7f5ddbddbcea9c3a17928758a011ea05fc2777b
EduardoVSR1995 <eduardvitor7@gmail.com>
2023-03-21 00:21:38 -0300

    feat: completed exercise Duplicates

--- DuplicadosNao/src/DuplicadosNao.java
+++ DuplicadosNao/src/DuplicadosNao.java
@@ -1,8 +1,17 @@
 public class DuplicadosNao{
-    int[] array1 = new int[]{1,2,3,4,5,6,7,44,55,66,77}; 
-    int[] array2 = new int[]{12,324,53,74,85,96,17,44,55,66,77}; 
+    int[] array1 = {1,2,3,4,5,6,7,44,55,66,77}; 
+    int[] array2 = {12,324,53,74,85,96,17,44,55,66,77}; 
     public static void main(String[] args) throws Exception {
-        
-        System.out.println("Hello, World!");
+        int[] arrayJob = new DuplicadosNao().array1;
+        int[] arrayIter = new DuplicadosNao().array2;
+        String value = "";
+        for(int i = 0 ; i < arrayJob.length ; i++ ){
+            for(int r = 0; r < arrayIter.length; r++ ){
+                if(arrayJob[i] == arrayIter[r]){
+                    value += arrayIter[r]+" ";
+                }
+            }   
+        }
+        System.out.println(value);
     }
 }
