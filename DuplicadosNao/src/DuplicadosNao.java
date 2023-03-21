f8fa38359a7daef2fa52cbc6e40096f6ba8c1bda
EduardoVSR1995 <eduardvitor7@gmail.com>
2023-03-21 14:18:53 -0300

    fix: refactory

--- DuplicadosNao/src/DuplicadosNao.java
+++ DuplicadosNao/src/DuplicadosNao.java
@@ -1,42 +1,9 @@
 import java.util.*;
 
 public class DuplicadosNao{
-    public static List array(boolean value){
-        List<Integer> array = new ArrayList<>(); 
-        for(int i = 0 ; i<=10000; i++){
-            if(value) {
-                array.add(new Random().nextInt(11));
-            }else{
-                array.add(i);
-            }
-        }
-
-        return array;
-    }
-    public static void main(String[] args){
-        List arrayJob = array(false);
-        List arrayIter = array(true);
-        Hashtable <String, Boolean> hash = new Hashtable<>();
+    public static void aninhad(List arrayJob, List arrayIter) {
         String value = "";
-        String stringJob = "";
-        String stringIter = "";
-        long timeInit = System.currentTimeMillis();
-        for(int i = 0 ; i < arrayJob.size() ; i++ ){
-            stringJob = ""+arrayJob.get(i);
-            stringIter = ""+arrayIter.get(i);
-            
-            hash.put(stringJob, true);
 
-            Boolean validation = hash.containsKey(stringIter);            
-            if(validation){ 
-               value += arrayIter.get(i)+" ";
-            }
-        }   
-        long timeEnd = System.currentTimeMillis() ;
-        
-        System.out.println("Quantidade de strings encontradas " +value.length() +". Tempo de execução hashtable: " +(timeEnd - timeInit));
-        
-        value = "";
         long timeInit1 = System.currentTimeMillis();
         for(int i = 0 ; i < arrayJob.size() ; i++ ){
             for(int r = i; r < arrayIter.size(); r++ ){
@@ -48,5 +15,48 @@ public class DuplicadosNao{
         long timeEnd1 = System.currentTimeMillis();
 
         System.out.println( "Quantidade de strings encontradas "+value.length()+ ". Tempo de execução com for aninhado: " +(timeEnd1 - timeInit1));
+    }
+
+    public static void hashTable(List arrayJob, List arrayIter){
+        Hashtable <String, Boolean> hash = new Hashtable<>();
+        String value = "";
+        String stringJob = "";
+        String stringIter = "";
+        long timeInit = System.currentTimeMillis();
+        for(int i = 0 ; i < arrayJob.size() ; i++ ){
+            stringJob = ""+arrayJob.get(i);
+            stringIter = ""+arrayIter.get(i);
+            
+            hash.put(stringJob, true);
+            Boolean validation = hash.containsKey(stringIter);            
+            if(validation){ 
+               value += arrayIter.get(i)+" ";
+            }
+        }   
+        long timeEnd = System.currentTimeMillis() ;
+            
+        System.out.println("Quantidade de strings encontradas " +value.length() +". Tempo de execução hashtable: " +(timeEnd - timeInit));    
+    }
+
+    public static List arrayDesorder(){
+        List<Integer> array = new ArrayList<>(); 
+        for(int i = 0 ; i<=10000; i++){
+            array.add(new Random().nextInt(11));
+        }
+        return array;
+    }
+    public static List arrayList(){
+        List<Integer> array = new ArrayList<>(); 
+        for(int i = 0 ; i<=10000; i++){
+            array.add(i);
+        }
+        return array;
+    }
+    public static void main(String[] args){
+        List arrayJob = arrayList();
+        List arrayIter = arrayDesorder();
+
+        hashTable(arrayJob, arrayIter);
+        aninhad(arrayJob, arrayIter);        
     }
 }
